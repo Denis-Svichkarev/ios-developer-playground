41bf9173e839269a8ce7403cbd3d0c169a1482b4
Denis Svichkarev <denis.svichkarev@gmail.com>
2024-11-23 21:47:29 +0100

    сhore: renamed projects

---
+++ StudySwift/StudySwift-SwiftTests/StudySwift_SwiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwift_SwiftTests.swift
+//  StudySwift-SwiftTests
+//
+//  Created by Denis Svichkarev on 20/11/24.
+//
+
+import Testing
+@testable import StudySwift_Swift
+
+struct StudySwift_SwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
